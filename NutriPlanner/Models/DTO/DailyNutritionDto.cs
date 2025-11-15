692d146939c4cebd76473cf3132c9377a87718de
kostylev.as2210 <kostylev.as2210@PC-6-ROOM409>
2025-11-15 14:51:05 +0500

    Добавлены DTO в проект

---
+++ NutriPlanner/Models/DTO/DailyNutritionDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriPlanner.Models.DTO
+{
+    public class DailyNutritionDto
+    {
+        public decimal TotalCalories { get; set; }
+        public decimal TotalProtein { get; set; }
+        public decimal TotalFat { get; set; }
+        public decimal TotalCarbs { get; set; }
+        public decimal TargetCalories { get; set; }
+        public decimal TargetProtein { get; set; }
+        public decimal TargetFat { get; set; }
+        public decimal TargetCarbs { get; set; }
+        public decimal CaloriesProgress { get; set; }
+        public decimal ProteinProgress { get; set; }
+        public decimal FatProgress { get; set; }
+        public decimal CarbsProgress { get; set; }
+
+        public ObservableCollection<MealDto> Meals { get; set; } = new ObservableCollection<MealDto>();
+    }
+}
---
+++ NutriPlanner/Models/DTO/UserRegistrationDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriPlanner.Models.DTO
+{
+    public class UserRegistrationDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public string ActivityLevel { get; set; } = string.Empty;
+        public string Goal { get; set; } = string.Empty;
+    }
+}
